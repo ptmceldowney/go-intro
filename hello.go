c34848753b8e319f1da9289a8381c9f74cfeae64
Patrick McEldowney <patrick@uhray.com>
2023-05-01 22:23:31 -0500

    Creating my first unit test

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+
+  "example/user/hello/morestrings"
+  "github.com/google/go-cmp/cmp"
+)
 
 func main() {
-  fmt.Println("Hello, World.")
+  fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+  fmt.Println(cmp.Diff("Hello Word", "Hello Go"))
 }
